32e5117f0d435b7773ec63a78d51afa07e2be89d
Badynko <surinalukas1991@gmail.com>
2024-05-25 18:19:15 +0200

    pridane obrazky a fetch

---
+++ .history/script_20240525180529.js
@@ -0,0 +1,2 @@
+//Unspolash API
+const apiUrl = `https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`;
---
+++ .history/script_20240525181101.js
@@ -0,0 +1,4 @@
+//Unspolash API
+const count = 10;
+const apiKey = "PgW2FbZj4v83yq_j7cjQ1SegR411cxSt5cA5XpIk-AQ";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
